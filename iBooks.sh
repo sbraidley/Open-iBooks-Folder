1f61af5a33081a3d8d480f5171d7dfed63b057f9
Sam Braidley <sbraidley@yahoo.co.uk>
2017-06-10 13:23:04 +0100

    Initial Commit
    Shell script to open hidden iBooks iCloud folder

---
+++ iBooks.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Open iBooks iCloud Folder
+# Sam Braidley - https://github.com/sbraidley/Open-iBooks-Folder
+
+cd ~/Library/Mobile\ Documents/iCloud\~com\~\apple\~iBooks/Documents
+open .
